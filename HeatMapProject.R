13e55632c714f58547a1063a83c89e59815181f9
Tristan Kaiser <tristanjkaiser@gmail.com>
2018-06-21 09:55:28 -0700

    Cleaned up code & added comments

--- HeatMapProject.R
+++ HeatMapProject.R
@@ -7,28 +7,33 @@ library(rworldmap)
 library(RColorBrewer)
 library(classInt)
 
-# Read in CSV file
-df <- read.csv('C:/Users/Tristan/Desktop/R Files/Data/ExampleHeatMapNew_TK.csv', header = TRUE, na.strings=c("", NA))
+# Load data ---------------------------
+df <- read.csv('./Data/Metal_Data.csv', header = TRUE, na.strings=c("", NA))
 
+
+# Clean data -------------------------
 # Convert Percentage column to numeric
-
+# You will run into a lot of issues here if the CSV file has a funky format
 df$Percentage <- as.numeric(sub("%", "", df$Percentage))
 
+
+# Build map -------------------------
 # Join data to map
 dfMap <- joinCountryData2Map(df
                              ,joinCode = "ISO3"
                              ,nameJoinColumn = "CountryCode"
-                             ,verbose=TRUE)
+                             ,verbose = TRUE)
 
 # Set Colours
 colourChoice <- palette(c("white", "#fee5d9", "#fcbba1", "#fc9272", "#fb6a4a", "#ef3b2c", "#cb181d", "#990000"))
 
 # Create categories for data
+# Breaks indicate the colour categories
 dfMap@data[["percentage.category"]] <- cut(dfMap@data[["Percentage"]],
                  breaks = c(-1, 0, 2, 5, 10, 20, 40, 60, 100),
-                 include.lowest=TRUE, na.rm=TRUE)
+                 include.lowest = TRUE, na.rm = TRUE)
 
-# Label the categories
+# Label the colour categories
 levels(dfMap@data[["percentage.category"]]) <- c("0%", "<2%",
                                     "=>2% - <5%",
                                     ">=5% - <10%",
@@ -38,24 +43,28 @@ levels(dfMap@data[["percentage.category"]]) <- c("0%", "<2%",
                                     ">=60% - 100%")
 
 # Open the Map Viewer
+# Only works in Windows
 mapDevice("x11")
 
 # Plot the data categories
-mapParams <- mapCountryData(dfMap
-               , nameColumnToPlot='percentage.category'
-               , catMethod='categorical'
-               , mapTitle= 'Global Copper Production 2013 - 18,500,000 tonnes'
-               , addLegend = FALSE
-               , colourPalette = colourChoice
-               , oceanCol='lightBlue',
-               , missingCountryCol='white')
+mapParams <- mapCountryData(dfMap,
+                            nameColumnToPlot = 'percentage.category',
+                            catMethod = 'categorical',
+                            mapTitle = 'Global Copper Production 2013 - 18,500,000 tonnes',
+                            addLegend = FALSE,
+                            colourPalette = colourChoice,
+                            oceanCol = 'lightBlue',
+                            missingCountryCol = 'white')
 
-# Add the Legend
+# Add the Legend -------------------------------
+# Current settings worked on X11 viewer
+# Adjust cex until the box fits properly
 mapParams$legendText <- levels(dfMap@data[["percentage.category"]])
-do.call(addMapLegendBoxes, c(mapParams,x='bottomleft',
-                             cex = .8,
-                             title="% of World Production",
-                             horiz=FALSE))
+do.call(addMapLegendBoxes, c(mapParams,x = 'bottomleft',
+                             cex = .6,
+                             title = "% of World Production",
+                             horiz = FALSE))
 
+# Modify these values until the source & box align correctly
 text(111,-85, "Source: USGS & Kaiser Research", cex = .8)
 rect(67,-80, 155, -90)
--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # Heatmap_rWorldMap
 First attempt at plotting a heatmap of metal production using rWorldMap
+
+Instructions:
+1) Session -> Set Working Directory -> To Source File Location
+
+2) Before running the first time, install the following packages:
+install.packages(c("rworldmap", "readr", "RColorBrewer", "classInt"))
+
+Put data files in ./Data
+
+Be sure to update the text in mapParams to align with the correct data (ie. Global Copper Production)
+each time you run the code.
+
+X11 will not work on Mac devices. I have not tested on a Windows device recently.
